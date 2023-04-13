24faad8e4e20ed4b07e4ca6a972f4e053e612e0d
lorenzocarlassara <lorenzo.carlassara98@gmail.com>
2023-04-13 15:48:20 +0200

    Create request.py

---
+++ request.py
@@ -0,0 +1,32 @@
+#Python sample for download of air quality up-to-date files
+#The Pyton script shows a simple sample how the pre-processed CSV files can be downloaded.
+#EEA takes no responsibility of the script and the code is provided 'as is', without warranty of any kind.
+#Peter Kjeld, 15. February 2019
+
+import requests
+
+print ('-----------------------------------------------------------------------')
+
+# Set download url
+ServiceUrl = "http://discomap.eea.europa.eu/map/fme/latest"
+
+# Countries to download
+# Note: List is not complete
+countries = ['SE']
+
+# Pollutant to be downloaded
+pollutants = ['PM10']
+
+for country in countries:
+	for pollutant in pollutants:
+		fileName = "%s_%s.csv" % (country, pollutant)
+		downloadFile = '%s/%s_%s.csv' % (ServiceUrl, country, pollutant)
+		#Download and save to local path
+		print('Downloading: %s' % downloadFile )
+		file = requests.get(downloadFile).content
+		output = open(fileName, 'wb')
+		output.write(file)
+		output.close()
+		print ('Saved locally as: %s ' % fileName)
+		print ('-----')
+	print ('Download finished')
