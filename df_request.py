034068c82b7f4c1b906b84ac277061ae78510985
lorenzocarlassara <lorenzo.carlassara98@gmail.com>
2023-04-13 17:09:52 +0200

    Create df_request.py

---
+++ df_request.py
@@ -0,0 +1,29 @@
+#Python sample for download of air quality up-to-date files
+#The Pyton script shows a simple sample how the pre-processed CSV files can be downloaded.
+#EEA takes no responsibility of the script and the code is provided 'as is', without warranty of any kind.
+#Peter Kjeld, 15. February 2019
+
+import requests
+
+def download_request(countries,pollutants):
+	print ('-----------------------------------------------------------------------')
+	# Set download url
+	ServiceUrl = "http://discomap.eea.europa.eu/map/fme/latest"
+
+	# country & pollutants list
+	'''https://discomap.eea.europa.eu/map/fme/latest/files.txt'''
+
+	for country in countries:
+		for pollutant in pollutants:
+			fileName = "%s_%s.csv" % (country, pollutant)
+			downloadFile = '%s/%s_%s.csv' % (ServiceUrl, country, pollutant)
+			#Download and save to local path
+			print('Downloading: %s' % downloadFile )
+			file = requests.get(downloadFile).content
+			output = open(fileName, 'wb')
+			output.write(file)
+			output.close()
+			print ('Saved locally as: %s ' % fileName)
+			print ('-----')
+		print ('Download finished')
+	return fileName
