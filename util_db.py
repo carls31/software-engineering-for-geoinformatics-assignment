24e7e95359be14899db9ec189cf84b871a9e1f39
lorenzocarlassara <lorenzo.carlassara98@gmail.com>
2023-05-22 09:48:35 +0200

    update

--- util_db.py
+++ util_db.py
@@ -1,5 +1,6 @@
 import ipywidgets as widgets
-from sqlalchemy import create_engine, text
+from IPython.display import display
+from sqlalchemy import create_engine
 
 
 def login():
@@ -33,3 +34,39 @@ def login():
         con = engine.connect()
         print('connected with localhost')
         return con'''
+def prova():
+    print('hello world')
+
+def login_required():
+    user = widgets.Text(
+        placeholder='Type postgres',
+        description='Username:',
+        disabled=False   
+    )
+
+    psw = widgets.Password(
+        placeholder='Enter password',
+        description='Password:',
+        disabled=False
+    )
+    
+    login_button = widgets.Button(description="Login")
+    display(user, psw, login_button)
+
+    def handle_login_button_click(button):
+        username = user.value
+        password = psw.value
+
+        # Check if username and password are valid
+        if username == "postgres" and password == "carIs3198":
+            # Connect to the database
+            engine = create_engine('postgresql://'+username.value+':'+password.value+'@localhost:5432/se4g') 
+            con = engine.connect()
+            # Perform any necessary database operations
+            # ...
+            # Return the database connection or perform any other actions
+            print('connected with localhost')
+            return con
+
+    login_button.on_click(handle_login_button_click)
+
