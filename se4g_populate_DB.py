6f72c8ae4e5d3b03c3283dc4533633d13b7db465
lorenzocarlassara <lorenzo.carlassara98@gmail.com>
2023-06-06 23:43:49 +0200

    Update se4g_populate_DB.py

--- se4g_populate_DB.py
+++ se4g_populate_DB.py
@@ -1,16 +1,6 @@
-from se4g_helper import download_DB
-import psycopg2
-ip = '192.168.30.19'
-ip = 'localhost'
-conn = psycopg2.connect(
-    host = ip,
-    database = "se4g",
-    user = "postgres",
-    password = "carIs3198"
-)
-print('connected with ',ip)
+from se4g_helper import download_DB, connect_right_now
+conn = connect_right_now()
 
 download_DB(connection=conn)
-
 
 conn.close()
