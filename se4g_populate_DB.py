8e97750c8e426704426aeb9dfd7476f298b909fa
lorenzocarlassara <lorenzo.carlassara98@gmail.com>
2023-06-08 09:42:46 +0200

    Update se4g_populate_DB.py

--- se4g_populate_DB.py
+++ se4g_populate_DB.py
@@ -1,6 +1,6 @@
 from se4g_helper import download_DB, connect_right_now
 conn = connect_right_now()
 
-download_DB(connection=conn)
+download_DB(connection=conn,table_name='se4g_pollution_main')
 
 conn.close()
