af0ec7dcdbd6b86bcc87642c1c1c60b72698b585
lorenzocarlassara <lorenzo.carlassara98@gmail.com>
2023-06-08 10:59:50 +0200

    Create se4g_dir.py

---
+++ db_helper/se4g_dir.py
@@ -0,0 +1,9 @@
+import os
+def set_the_working_directory():
+    current_dir = os.getcwd()
+    print('Current directory: ',current_dir)
+    parent_dir = os.path.dirname(current_dir)
+
+    os.chdir(parent_dir)
+
+    print('Current working directory:', os.getcwd())
