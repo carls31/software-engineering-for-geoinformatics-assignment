a08ced2ba177ac6599500dfb6d1726d8b5d225a6
lorenzocarlassara <lorenzo.carlassara98@gmail.com>
2023-06-13 15:48:22 +0200

    Update se4g_populate_DB.py

--- se4g_populate_DB.py
+++ se4g_populate_DB.py
@@ -1,6 +1,20 @@
-from se4g_helper import download_DB, connect_right_now
+from se4g_helper import (download_DB, connect_right_now,
+                        connect_with_sqlalchemy, update_dashboard_table)
 conn = connect_right_now()
 
 download_DB(connection=conn,table_name='se4g_pollution_main')
 
-conn.close() 
+'''conn.close() '''
+
+'''dataset = 'se4g_dashboard_dataset.csv' # - 1.2s
+folder = 'data_prova' '''
+
+# Update dashboard table
+
+'''conn = connect_right_now()'''
+engine = connect_with_sqlalchemy()
+update_dashboard_table()
+
+
+conn.close()
+engine.dispose()
