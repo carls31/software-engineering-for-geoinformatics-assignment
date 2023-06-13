8c4ac03807e73b647c97c7876a26e1f675584943
lorenzocarlassara <lorenzo.carlassara98@gmail.com>
2023-06-13 15:48:25 +0200

    Update se4g_populate_dataset.py

--- se4g_populate_dataset.py
+++ se4g_populate_dataset.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
-from se4g_helper import (download_request, build_dataframe, update_dataset, update_dashboard_dataset,
-                         connect_right_now, connect_with_sqlalchemy)
+from se4g_helper import (download_request, build_dataframe, update_dataset, update_dashboard_dataset)
 # Download and get the dataframe file name
 dir = download_request(folder_out = 'data_prova')
 #dir = '2023-06-09_14_50_48'
@@ -12,29 +11,6 @@ df = build_dataframe(dir, folder_out = 'data_prova')
 # Update the main dataset & the dashboard dataset 
 new_df = update_dataset(df, folder_out = 'data_prova')
 update_dashboard_dataset(df, folder_out = 'data_prova')
-
-dataset = 'se4g_dashboard_dataset.csv' # - 1.2s
-folder = 'data_prova'
-
-# Update dashboard table
-conn = connect_right_now()
-engine = connect_with_sqlalchemy()
-
-cur = conn.cursor()
-
-table_name = "se4g_dashboard"
-cur.execute(f"DROP TABLE IF EXISTS {table_name}")
-
-conn.commit()
-
-cur.close()
-conn.close()
-
-full_path = os.path.join(os.getcwd(),folder,dataset)
-df = pd.read_csv(full_path)
-df.to_sql(table_name, engine, if_exists = 'append', index=False)
-
-engine.dispose()
 
 
 # Update DB
