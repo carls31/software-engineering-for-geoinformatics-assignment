31a540ea6c8e568705af1fc552c3f9a2743c7137
lorenzocarlassara <lorenzo.carlassara98@gmail.com>
2023-06-15 10:03:39 +0200

    Update se4g_populate_DB.py

--- se4g_populate_DB.py
+++ se4g_populate_DB.py
@@ -1,5 +1,9 @@
-from se4g_helper import download_DB, update_dashboard_table
-download_DB()
+from se4g_helper import (download_DB, update_dashboard_table, 
+                         connect_right_now, connect_with_sqlalchemy)
+download_DB( conn=connect_right_now() )
  
 # Update dashboard table
-update_dashboard_table()
+update_dashboard_table(
+                        conn=connect_right_now(), 
+                        engine=connect_with_sqlalchemy()
+                        )
