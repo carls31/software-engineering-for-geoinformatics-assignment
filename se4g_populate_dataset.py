6c0c1ed1ab991ed3e51191f14e51cf73ed4d634e
lorenzocarlassara <lorenzo.carlassara98@gmail.com>
2023-06-15 09:45:50 +0200

    Update se4g_populate_dataset.py

--- se4g_populate_dataset.py
+++ se4g_populate_dataset.py
@@ -1,5 +1,3 @@
-import os
-import pandas as pd
 from se4g_helper import (download_request, build_dataframe, update_dataset, update_dashboard_dataset)
 # Download and get the dataframe file name
 dir = download_request(folder_out = 'data_prova')
