df9e100aa98629ff407b57e8a1cb877f6b2a7709
lorenzocarlassara <lorenzo.carlassara98@gmail.com>
2023-06-15 14:22:45 +0200

    Update se4g_dir.py

--- helper/se4g_dir.py
+++ helper/se4g_dir.py
@@ -1,7 +1,7 @@
 import os
 def set_the_working_directory():
     current_dir = os.getcwd()
-    print('Current directory: ',current_dir)
+    #print('Current directory: ',current_dir)
     parent_dir = os.path.dirname(current_dir)
 
     os.chdir(parent_dir)
