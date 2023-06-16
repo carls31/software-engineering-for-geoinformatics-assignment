e6a0cef78c38601f008013f1622a7dd476660ae7
lorenzocarlassara <lorenzo.carlassara98@gmail.com>
2023-06-16 18:32:26 +0200

    Update se4g_helper.py

--- se4g_helper.py
+++ se4g_helper.py
@@ -514,11 +514,37 @@ def update_dashboard_dataset(df,folder_out = 'data'):
          df.to_csv(full_path, index=False)
          print("Dataset ->",fileName," created successfully")
         
+#---------------------------------------------------------------------------------------------------------------------#
 
      
-     
+def update_data():
+    # Download and get the dataframe file name
+    dir = download_request(folder_out = 'data_prova')
+    #dir = '2023-06-09_14_50_48'
+
+    # Build the dataframe with the required structure
+    df = build_dataframe(dir, folder_out = 'data_prova')
+
+    # Update the main dataset & the dashboard dataset 
+    new_df = update_dataset(df, folder_out = 'data_prova')
+    update_dashboard_dataset(df, folder_out = 'data_prova')
+
+    # Update DB
+
+    download_DB( conn=connect_right_now() )
+    
+    # Update dashboard table
+    update_dashboard_table( conn=connect_right_now(), 
+                            engine=connect_with_sqlalchemy()
+                            )
+    '''filtered_rows = update_DB_from_CSV(df, conn, conn, table_name = 'se4g_pollution') # new_df
+    update_dashboard_DB_from_CSV(filtered_rows, conn, conn, table_name = 'se4g_dashboard')
+    conn.close()'''   
 
 	
+#---------------------------------------------------------------------------------------------------------------------#
+
+
 
 #---------------------------------------------------------------------------------------------------------------------#
 #---------------------------------------------------------------------------------------------------------------------#
