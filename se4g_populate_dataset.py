6da2fe0bb38e8bd8d7ccdd49ec216ddd63afff44
lorenzocarlassara <lorenzo.carlassara98@gmail.com>
2023-06-16 18:32:25 +0200

    Update se4g_populate_dataset.py

--- se4g_populate_dataset.py
+++ se4g_populate_dataset.py
@@ -1,4 +1,7 @@
-from se4g_helper import (download_request, build_dataframe, update_dataset, update_dashboard_dataset)
+from se4g_helper import update_data
+
+'''from se4g_helper import (download_request, build_dataframe, update_dataset, update_dashboard_dataset,
+                         download_DB, update_dashboard_table, connect_right_now, connect_with_sqlalchemy)
 def update_data():
     # Download and get the dataframe file name
     dir = download_request(folder_out = 'data_prova')
@@ -11,18 +14,13 @@ def update_data():
     new_df = update_dataset(df, folder_out = 'data_prova')
     update_dashboard_dataset(df, folder_out = 'data_prova')
 
-
     # Update DB
-    '''filtered_rows = update_DB_from_CSV(df, conn, conn, table_name = 'se4g_pollution') # new_df
-    update_dashboard_DB_from_CSV(filtered_rows, conn, conn, table_name = 'se4g_dashboard')
-    conn.close()'''
 
-    from se4g_helper import (download_DB, update_dashboard_table, 
-                            connect_right_now, connect_with_sqlalchemy)
     download_DB( conn=connect_right_now() )
     
     # Update dashboard table
     update_dashboard_table( conn=connect_right_now(), 
                             engine=connect_with_sqlalchemy()
-                            )
+                            )'''
+
 update_data()
