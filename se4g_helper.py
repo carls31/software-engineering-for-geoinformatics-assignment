830f6c385053fed5aada8656d63b213ef12b4c16
lorenzocarlassara <lorenzo.carlassara98@gmail.com>
2023-06-18 23:56:26 +0200

    login

--- se4g_helper.py
+++ se4g_helper.py
@@ -535,28 +535,41 @@ def update_dashboard_dataset(df,folder_out = 'data'):
 
      
 def update_data():
-    # Download and get the dataframe file name
-    dir = download_request(folder_out = 'data_prova')
-    #dir = '2023-06-09_14_50_48'
+    if Login().logged_in:
+        # Download and get the dataframe file name
+        dir = download_request(folder_out = 'data_prova')
+        #dir = '2023-06-09_14_50_48'
 
-    # Build the dataframe with the required structure
-    df = build_dataframe(dir, folder_out = 'data_prova')
+        # Build the dataframe with the required structure
+        df = build_dataframe(dir, folder_out = 'data_prova')
 
-    # Update the main dataset & the dashboard dataset 
-    new_df = update_dataset(df, folder_out = 'data_prova')
-    update_dashboard_dataset(df, folder_out = 'data_prova')
+        # Update the main dataset & the dashboard dataset 
+        new_df = update_dataset(df, folder_out = 'data_prova')
+        update_dashboard_dataset(df, folder_out = 'data_prova')
 
-    # Update DB
+        # Update DB
 
-    download_DB( conn=connect_right_now() )
-    
-    # Update dashboard table
-    update_dashboard_table( conn=connect_right_now(), 
-                            engine=connect_with_sqlalchemy()
-                            )
+        download_DB( conn=connect_right_now() )
+        
+        # Update dashboard table
+        update_dashboard_table( conn=connect_right_now(), 
+                                engine=connect_with_sqlalchemy()
+                                )
+    else:
+        print("You need to be logged in to access this feature.")
     '''filtered_rows = update_DB_from_CSV(df, conn, conn, table_name = 'se4g_pollution') # new_df
     update_dashboard_DB_from_CSV(filtered_rows, conn, conn, table_name = 'se4g_dashboard')
     conn.close()'''   
+
+
+def logged_in_example():
+    if Login().logged_in:
+        print('congratulations! You are logged in!')
+    else:
+        print("You need to be logged in to access this feature.")
+   
+
+
 
 	
 #---------------------------------------------------------------------------------------------------------------------#
@@ -630,6 +643,15 @@ class Login:
         if os.path.isfile(self.path_file):
             df = pd.read_csv(self.path_file)
             self.user_list = dict(zip(df['Username'], df['Password']))
+        self.logged_in = False
+
+    def authenticate(self):
+        # Add code for the login process
+        self.logged_in = True
+
+    def logout(self):
+        # Add code for the logout process
+        self.logged_in = False
 
     def check_credentials(self, username, password):
         if username in self.user_list and self.user_list[username] == password:
@@ -698,6 +720,7 @@ class Login:
             password = psw.value
 
             if self.check_credentials(username, password):
+                self.authenticate()
                 print("Login successful!")
                 # Perform the desired actions here after successful login
             else:
@@ -705,7 +728,35 @@ class Login:
 
         login_button.on_click(handle_login_button_click)
 
+def login_register_section():
+    register = Register()
+    login = Login()
+    if login.logged_in:
+        print("You are logged in.")
+        action = input("Choose an action (1 - Logout, 2 - Perform Authorized Action, 3 - Exit): ")
 
+        if action == "1":
+            login.logout()
+            print("Logged out successfully.")
+        elif action == "2":
+            # Include code for authorized functions/cells
+            print("Performing authorized action...")
+        elif action == "3":
+            print("Exiting...")
+        else:
+            print("Invalid action.")
+    else:
+        print("You are not logged in.")
+        action = input("Choose an action (1 - Login, 2 - Register, 3 - Exit): ")
+
+        if action == "1":
+            login._required()
+        elif action == "2":
+            register._new_user()
+        elif action == "3":
+            print("Exiting...")
+        else:
+            print("Invalid action.")
 
 #---------------------------------------------------------------------------------------------------------------------#
 #---------------------------------------------------------------------------------------------------------------------#
